a27ecf303954dc2a69187e360b70116bdcd4eb32
Anton Holovchenko <ah@Antons-MacBook-Pro.local>
2025-05-03 00:14:24 +0200

    Upd cloc module with correct output

--- src/modules/cloc_module.py
+++ src/modules/cloc_module.py
@@ -22,7 +22,7 @@ class Cloc(Log):
         self.repo_path: str = repo_path
     
 
-    def count_lines_of_code_full_scope(self, framework: str) -> dict:
+    def count_lines_of_code_full_scope(self, framework: str) -> str:
         """
         Count the lines of code in the solidity files for the given framework in the full scope of the repository.
         The total lines of code is returned.
@@ -31,16 +31,17 @@ class Cloc(Log):
             raise ValueError(f"Framework {framework} not found in available frameworks")
         
         exclusion: str = "|".join(self.CLOC_DEFINITIONS[framework]["exclude"])
+        
         try:
             self.log_info("Counting lines of code...\n")
-            result: dict = subprocess.check_output(
-                ["cloc", f"--include-ext={self.CLOC_DEFINITIONS[framework]["extension"]}", self.repo_path, "--by-file", f"--not-match-d={exclusion}"],
+            result: str = subprocess.run(
+                ["cloc", f"--include-ext={self.CLOC_DEFINITIONS[framework]["extension"]}", ".", "--by-file", f"--not-match-d={exclusion}"],
                 cwd=self.repo_path,
+                capture_output=True,
                 text=True
             )
-            self.log_info(result)
-            return result
-        except subprocess.CalledProcessError as e:
-            self.log_error("Error running cloc: ", str(e))
+            self.log_info(result.stdout)
+            return result.stdout
+        except subprocess.CalledProcessError as clocException:
+            self.log_error("Error running cloc: ", str(clocException))
             return {}
-    
